0f73baed799994bfc7aa135317dad1392cd75d7d
Matus Sabo <matus.edko.sabo@gmail.com>
2023-08-08 09:24:42 +0200

    update evsieve

--- evsieve/evsieve-fedora.sh
+++ evsieve/evsieve-fedora.sh
@@ -5,17 +5,17 @@ source "evsieve/evsieve.conf"
 evsieve --input  $KEYBOARD_INPUT_1 domain=kb1 grab \
         --input  $KEYBAORD_INPUT_2 domain=kb2 grab \
         --input  $MOUSE_INPUT domain=ms grab \
-        --toggle @kb1 @vm-kb @host-kb \
-        --toggle @kb2 @vm-kb @host-kb \
-        --toggle @ms @vm-ms @host-ms \
-        --hook   key:scrolllock key:h sequential toggle=:2 exec-shell="evsieve/toggle.sh host" send-key=key:esc@host-kb \
-        --hook   key:scrolllock key:v sequential toggle=:1 exec-shell="evsieve/toggle.sh vm" send-key=key:esc@vm-kb \
+        --toggle @kb1 @kb-vm @kb-host \
+        --toggle @kb2 @kb-vm @kb-host \
+        --toggle @ms @ms-vm @ms-host \
+        --hook   key:scrolllock key:h sequential toggle=:2 exec-shell="evsieve/toggle.sh host" send-key=key:esc@kb-host \
+        --hook   key:scrolllock key:v sequential toggle=:1 exec-shell="evsieve/toggle.sh vm" send-key=key:esc@kb-vm \
         --withhold \
-        --hook   key:scrolllock@vm-kb toggle=:2 exec-shell="evsieve/toggle.sh host" send-key=key:esc@host-kb \
-        --hook   key:scrolllock@host-kb toggle=:1 exec-shell="evsieve/toggle.sh vm" send-key=key:esc@vm-kb \
-        --map    key:leftalt@host-kb key:leftctrl \
-        --map    key:leftalt@vm-kb key:leftctrl \
-        --output @host-kb create-link=$KEYBOARD_HOST_INPUT \
-        --output @vm-kb create-link=$KEYBOARD_VM_INPUT \
-        --output @host-ms create-link=$MOUSE_HOST_INPUT \
-        --output @vm-ms create-link=$MOUSE_VM_INPUT
+        --hook   key:scrolllock@kb-vm toggle=:2 exec-shell="evsieve/toggle.sh host" send-key=key:esc@kb-host \
+        --hook   key:scrolllock@kb-host toggle=:1 exec-shell="evsieve/toggle.sh vm" send-key=key:esc@kb-vm \
+        --map    key:leftalt@kb-host key:leftctrl \
+        --map    key:leftalt@kb-vm key:leftctrl \
+        --output @kb-host create-link=$KEYBOARD_HOST_INPUT \
+        --output @kb-vm create-link=$KEYBOARD_VM_INPUT \
+        --output @ms-host create-link=$MOUSE_HOST_INPUT \
+        --output @ms-vm create-link=$MOUSE_VM_INPUT
--- evsieve/evsieve-host.sh
+++ evsieve/evsieve-host.sh
@@ -5,16 +5,16 @@ source "evsieve/evsieve.conf"
 evsieve --input  $KEYBOARD_INPUT_1 domain=kb1 grab \
         --input  $KEYBAORD_INPUT_2 domain=kb2 grab \
         --input  $MOUSE_INPUT domain=ms grab \
-        --toggle @kb1 @host-kb @vm-kb \
-        --toggle @kb2 @host-kb @vm-kb \
-        --toggle @ms @host-ms @vm-ms \
-        --hook   key:scrolllock key:h sequential toggle=:1 exec-shell="evsieve/toggle.sh host" send-key=key:esc@host-kb \
-        --hook   key:scrolllock key:v sequential toggle=:2 exec-shell="evsieve/toggle.sh vm" send-key=key:esc@vm-kb \
+        --toggle @kb1 @kb-host @kb-vm \
+        --toggle @kb2 @kb-host @kb-vm \
+        --toggle @ms @ms-host @ms-vm \
+        --hook   key:scrolllock key:h sequential toggle=:1 exec-shell="evsieve/toggle.sh host" send-key=key:esc@kb-host \
+        --hook   key:scrolllock key:v sequential toggle=:2 exec-shell="evsieve/toggle.sh vm" send-key=key:esc@kb-vm \
         --withhold \
-        --hook   key:scrolllock@vm-kb toggle=:1 exec-shell="evsieve/toggle.sh host" send-key=key:esc@host-kb \
-        --hook   key:scrolllock@host-kb toggle=:2 exec-shell="evsieve/toggle.sh vm" send-key=key:esc@vm-kb \
-        --map    key:leftalt@host-kb key:leftctrl \
-        --output @host-kb create-link=$KEYBOARD_HOST_INPUT \
-        --output @vm-kb create-link=$KEYBOARD_VM_INPUT \
-        --output @host-ms create-link=$MOUSE_HOST_INPUT \
-        --output @vm-ms create-link=$MOUSE_VM_INPUT
+        --hook   key:scrolllock@kb-vm toggle=:1 exec-shell="evsieve/toggle.sh host" send-key=key:esc@kb-host \
+        --hook   key:scrolllock@kb-host toggle=:2 exec-shell="evsieve/toggle.sh vm" send-key=key:esc@kb-vm \
+        --map    key:leftalt@kb-host key:leftctrl \
+        --output @kb-host create-link=$KEYBOARD_HOST_INPUT \
+        --output @kb-vm create-link=$KEYBOARD_VM_INPUT \
+        --output @ms-host create-link=$MOUSE_HOST_INPUT \
+        --output @ms-vm create-link=$MOUSE_VM_INPUT
